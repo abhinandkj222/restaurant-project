655264bca188beb303a88105f6e0047b5b572744
ABHINAND K J <abhinand.k@consciotech.com>
2026-08-14 20:55:25 +0530

    configure server for render

--- restaurant-server/src/server.js
+++ restaurant-server/src/server.js
@@ -9,7 +9,7 @@ db.one('SELECT 1')
   .then(() => {
     console.log('✅ PostgreSQL Connected');
 
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`🚀 Server running on port ${PORT}`);
     });
   })
