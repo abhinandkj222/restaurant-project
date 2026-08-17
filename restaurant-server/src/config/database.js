bf550c191fd3ef77966edd6c43cc67c29e9c0e59
ABHINAND K J <abhinand.k@consciotech.com>
2026-08-17 17:58:11 +0530

    fix database config syntax

--- restaurant-server/src/config/database.js
+++ restaurant-server/src/config/database.js
@@ -10,17 +10,5 @@ const db = pgp({
     rejectUnauthorized: false,
   },
 });
-=
+
 module.exports = db;
-
-// const pgp = require('pg-promise')();
-
-// const db = pgp({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
-
-// module.exports = db;
